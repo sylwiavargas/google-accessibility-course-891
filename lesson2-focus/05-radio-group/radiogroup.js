14ed14e91ba0fc3fb21713a45696c115eb05c93a
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-06 23:01:58 -0400

    done 02-05

--- lesson2-focus/05-radio-group/radiogroup.js
+++ lesson2-focus/05-radio-group/radiogroup.js
@@ -33,6 +33,12 @@
 
         // This seems like a good place to do some stuff :)
 
+      if (this.focusedIdx === 0) {
+        this.focusedIdx = this.buttons.length - 1;
+      } else {
+        this.focusedIdx--;
+      }
+
         break;
 
       }
@@ -44,6 +50,11 @@
 
         // This seems like a good place to do some stuff :)
 
+      if (this.focusedIdx === this.buttons.length - 1) {
+        this.focusedIdx = 0;
+      } else {
+        this.focusedIdx++;
+      }
         break;
       }
 
